2a6437ca36a01895d972527a02f6dfb7cb31a9ab
Alex <js.fullstack.development@gmail.com>
2021-06-13 23:12:18 +0300

    hw

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const {
-  postContactsValidation,
+  postContactsValidation
   putContactsValidation,
   patchContactsValidation
 } = require('../../middlewears/contactsValidation')
